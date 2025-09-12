e5bb6a297557877611122fa1510e50a874459536
jenilsabhadiya <sabhadiyajenil61@gmail.com>
2025-09-12 12:34:37 +0530

    12_09_2025_Dashboard_StatCard

--- src/Admin/container/Dashboard/Dashboard.jsx
+++ src/Admin/container/Dashboard/Dashboard.jsx
@@ -9,6 +9,17 @@ function Dashboard() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
+  const [showHighlight, setShowHighlight] = React.useState(true);
+  const [showTooltip, setShowTooltip] = React.useState(true);
+
+  const handleHighlightChange = (event) => {
+    setShowHighlight(event.target.checked);
+  };
+
+  const handleTooltipChange = (event) => {
+    setShowTooltip(event.target.checked);
+  };
+
   const stats = [
     {
       title: "Total active users",
@@ -17,6 +28,7 @@ function Dashboard() {
       status: true,
       color: colors.primary[500],
       data: [1, 4, 2, 5, 7, 2, 4, 6],
+      dataColor: colors.primary[500],
     },
     {
       title: "Total installed",
@@ -24,7 +36,8 @@ function Dashboard() {
       change: "+0.2%",
       status: true,
       color: colors.primary[800],
-      data: [1, 4, 2, 5, 7, 2, 4, 6],
+      data: [8, 5, 2, 5, 9, 2, 8, 1],
+      dataColor: colors.primary[900],
     },
     {
       title: "Total downloads",
@@ -33,6 +46,7 @@ function Dashboard() {
       status: false,
       color: colors.primary[100],
       data: [1, 4, 2, 5, 7, 2, 4, 6],
+      dataColor: colors.primary[100],
     },
     {
       title: "Custom stat",
@@ -40,40 +54,58 @@ function Dashboard() {
       change: "+1.0%",
       status: true,
       color: colors.primary[300],
-      data: [1, 4, 2, 5, 7, 2, 4, 6],
+      data: [8, 5, 2, 5, 9, 2, 8, 1],
+      dataColor: colors.primary[400],
     },
   ];
 
-  const StatCard = ({ title, value, change, status, color, data }) => (
+  const StatCard = ({
+    title,
+    value,
+    change,
+    status,
+    color,
+    data,
+    dataColor,
+  }) => (
     <Paper
       elevation={3}
       sx={{
         p: 3,
         borderRadius: 3,
-        height: "100%",
       }}
     >
-      <Typography variant="subtitle2" color="text.secondary">
+      <Typography variant="subtitle2" color="text.primary">
         {title}
       </Typography>
 
-      <Typography
-        variant="h4"
-        fontWeight={700}
-        sx={{ mt: 1, display: "flex", justifyContent: "space-between" }}
-      >
-        {value}
-
-        <SparkLineChart
-          plotType="bar"
-          data={data}
-          height={50}
-          width={70}
-          showHighlight={false}
-          showTooltip={false}
-          sx={{ mt: 1 }}
-        />
-      </Typography>
+      <Grid container spacing={2}>
+        <Grid
+          size={8}
+          sx={{
+            mt: 1,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "self-end",
+          }}
+        >
+          <Typography color="text.primary" variant="h4" fontWeight={700}>
+            {value}
+          </Typography>
+        </Grid>
+        <Grid size={4}>
+          <SparkLineChart
+            plotType="bar"
+            data={data}
+            height={50}
+            width={70}
+            showHighlight={showHighlight}
+            showTooltip={showTooltip}
+            color={dataColor}
+            sx={{ mt: 1 }}
+          />
+        </Grid>
+      </Grid>
 
       <Box display="flex" alignItems="center" color={color}>
         {status ? (
@@ -87,8 +119,9 @@ function Dashboard() {
       </Box>
     </Paper>
   );
+
   return (
-    <Box sx={{ p: 2, backgroundColor: "#f8fafc", minHeight: "100vh" }}>
+    <Box sx={{ p: 2, minHeight: "100vh" }}>
       <Grid container spacing={2}>
         {stats.map((v, i) => (
           <Grid size={{ xs: 12, md: 6, lg: 3 }} key={i}>
